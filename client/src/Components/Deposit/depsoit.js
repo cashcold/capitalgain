8d797138c42f7f9801c2a9ca1361444fe407c6b3
cashcold <cashcold99@gmail.com>
2021-06-28 21:47:24 +0000

    working deposit

--- client/src/Components/Deposit/depsoit.js
+++ client/src/Components/Deposit/depsoit.js
@@ -8,34 +8,144 @@ class DepositMain extends Component {
     render() { 
         return ( 
             <div className='depositMain'>
+                <h1 className='newDeposit'>NEW <span>DEPOSIT</span></h1>
+                <div className="allSection">
                 <section className='deposit__box__1'>
                     <div className="deposit__1">
-                        <h1>NEW DEPOSIT</h1>
                         <div className="depositInfo__lay__1">
-                            <h3>Plan 1</h3>
+                            <h3> <input type='radio' name='planNow'   className='planBtn4'/>  Plan 1</h3>
                         </div>
                         <div className="depositInfo__lay__2">
                              <div className="depositInfo__box__1">
                                  <div className="depositInfo__box__1">
                                     <div className="depositInfo__innerbox__1">
-                                        <h4>Plan</h4>
-                                        <h4>Spent Amount ($)</h4>
-                                        <h4>Profit (%)</h4>
+                                        <h4><span>Plan</span></h4>
+                                        <h4><span>Spent Amount ($)</span></h4>
+                                        <h4><span>Profit (%)</span></h4>
                                     </div>
                                  </div>
                              </div> 
                              <div className="depositInfo__box__2">
                                      <div className="depositInfo__box__1">
                                         <div className="depositInfo__innerbox__1">
-                                            <h4>Plan 1</h4>
+                                            <h4>24HRS</h4>
                                             <h4>$20.00 - $50000.00	</h4>
-                                            <h4>9.5</h4>
+                                            <h4>9.5<br/><a href=''>Calculate your profit</a></h4>
                                         </div>
                                     </div>
                              </div>
                         </div>
                     </div>
                 </section>
+                <section className='deposit__box__1'>
+                    <div className="deposit__1">
+                        <div className="depositInfo__lay__1">
+                            <h3> <input type='radio' name='planNow'   className='planBtn4'/>  Plan 2</h3>
+                        </div>
+                        <div className="depositInfo__lay__2">
+                             <div className="depositInfo__box__1">
+                                 <div className="depositInfo__box__1">
+                                    <div className="depositInfo__innerbox__1">
+                                        <h4><span>Plan</span></h4>
+                                        <h4><span>Spent Amount ($)</span></h4>
+                                        <h4><span>Profit (%)</span></h4>
+                                    </div>
+                                 </div>
+                             </div> 
+                             <div className="depositInfo__box__2">
+                                     <div className="depositInfo__box__1">
+                                        <div className="depositInfo__innerbox__1">
+                                            <h4>Plan 2</h4>
+                                            <h4>$20.00 - $50000.00	</h4>
+                                            <h4>9.5<br/><a href=''>Calculate your profit</a></h4>
+                                        </div>
+                                    </div>
+                             </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='deposit__box__1'>
+                    <div className="deposit__1">
+                        <div className="depositInfo__lay__1">
+                            <h3> <input type='radio' name='planNow'   className='planBtn4'/>  Plan 3</h3>
+                        </div>
+                        <div className="depositInfo__lay__2">
+                             <div className="depositInfo__box__1">
+                                 <div className="depositInfo__box__1">
+                                    <div className="depositInfo__innerbox__1">
+                                        <h4><span>Plan</span></h4>
+                                        <h4><span>Spent Amount ($)</span></h4>
+                                        <h4><span>Profit (%)</span></h4>
+                                    </div>
+                                 </div>
+                             </div> 
+                             <div className="depositInfo__box__2">
+                                     <div className="depositInfo__box__1">
+                                        <div className="depositInfo__innerbox__1">
+                                            <h4>Plan 3</h4>
+                                            <h4>$20.00 - $50000.00	</h4>
+                                            <h4>9.5<br/><a href=''>Calculate your profit</a></h4>
+                                        </div>
+                                    </div>
+                             </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='deposit__box__1'>
+                    <div className="deposit__1">
+                        <div className="depositInfo__lay__1">
+                            <h3> <input type='radio' name='planNow'   className='planBtn4'/>  Plan 4</h3>
+                        </div>
+                        <div className="depositInfo__lay__2">
+                             <div className="depositInfo__box__1">
+                                 <div className="depositInfo__box__1">
+                                    <div className="depositInfo__innerbox__1">
+                                        <h4><span>Plan</span></h4>
+                                        <h4><span>Spent Amount ($)</span></h4>
+                                        <h4><span>Profit (%)</span></h4>
+                                    </div>
+                                 </div>
+                             </div> 
+                             <div className="depositInfo__box__2">
+                                     <div className="depositInfo__box__1">
+                                        <div className="depositInfo__innerbox__1">
+                                            <h4>Plan 4</h4>
+                                            <h4>$20.00 - $50000.00	</h4>
+                                            <h4>9.5<br/><a href=''>Calculate your profit</a></h4>
+                                        </div>
+                                    </div>
+                             </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='deposit__box__1'>
+                    <div className="deposit__1">
+                        <div className="depositInfo__lay__1">
+                            <h3> <input type='radio' name='planNow'   className='planBtn4'/>  Plan 5</h3>
+                        </div>
+                        <div className="depositInfo__lay__2">
+                             <div className="depositInfo__box__1">
+                                 <div className="depositInfo__box__1">
+                                    <div className="depositInfo__innerbox__1">
+                                        <h4><span>Plan</span></h4>
+                                        <h4><span>Spent Amount ($)</span></h4>
+                                        <h4><span>Profit (%)</span></h4>
+                                    </div>
+                                 </div>
+                             </div> 
+                             <div className="depositInfo__box__2">
+                                     <div className="depositInfo__box__1">
+                                        <div className="depositInfo__innerbox__1">
+                                            <h4>Plan 5</h4>
+                                            <h4>$20.00 - $50000.00	</h4>
+                                            <h4>9.5<br/><a href=''>Calculate your profit</a></h4>
+                                        </div>
+                                    </div>
+                             </div>
+                        </div>
+                    </div>
+                </section>
+                </div>
             </div>
          );
     }
